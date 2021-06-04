3e4fbcb257812189edaac5cd67c8362eb0f4177d
Galeno de Melo <galenodemelo@gmail.com>
2021-06-03 22:59:40 -0300

    feat: Adicionando segundo slider de área externa
    # Refatorado o component de Slider para ser reutilizado por ambos os sliders

--- components/PictureSlider.js
+++ components/PictureSlider.js
@@ -1,38 +1,26 @@
 import Image from "next/image"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import Glide from "@glidejs/glide"
 
-export default function PictureSlider() {
+export default function PictureSlider({imageList}) {
+    const slider = useRef()
 
     useEffect(() => {
-        new Glide(".glide--picture-slider", {
-            gap: 0
-        }).mount()
+        new Glide(slider.current, { gap: 0 }).mount()
     })
 
     return (
         <section className="panel">
-            <div className="glide glide--picture-slider glide--full-height">
+            <div className="glide glide--full-height" ref={slider}>
                 <div className="glide__track" data-glide-el="track">
                     <ul className="glide__slides">
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/party-room.jpg" layout="fill" objectFit="cover" alt="Foto do salão de festas com sofá, televisão, mesas e cadeiras" loading="lazy" />
-                        </li>
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/play-room.jpg" layout="fill" objectFit="cover" alt="Foto do salão de jogos com mesa de bilhar, bar e mesa para cartas" loading="lazy" />
-                        </li>
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/theater.jpg" layout="fill" objectFit="cover" alt="Foto da sala de cinema, com sofá grande, puffs e telão" loading="lazy" />
-                        </li>
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/pool.jpg" layout="fill" objectFit="cover" alt="Foto da área de piscina com aquecimento" loading="lazy" />
-                        </li>
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/gym.jpg" layout="fill" objectFit="cover" alt="Foto da academia com halteres, esteiras e outros equipamentos" loading="lazy" />
-                        </li>
-                        <li className="glide__slide">
-                            <Image src="/img/sliders/pictures/kids-place.jpg" layout="fill" objectFit="cover" alt="Foto da área para crianças, com piscina de bolinhas, TV e playground" loading="lazy" />
-                        </li>
+                        {imageList.map((item, index) => {
+                            return (
+                                <li className="glide__slide" key={index}>
+                                    <Image src={item.url} layout="fill" objectFit="cover" alt={item.alt} loading="lazy" />
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Opening from "../components/Opening"
 import PictureSlider from "../components/PictureSlider"
 import Sophistication from "../components/Sophistication"
 
-export default function Home() {
+export default function Home(props) {
     return (
         <>
             <Head>
@@ -27,7 +27,78 @@ export default function Home() {
             </section>
 
             <Sophistication/>
+            <PictureSlider imageList={props.commonAreas1}/>
             <Naturality/>
+            <PictureSlider imageList={props.commonAreas2}/>
         </>
     )
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            // First slider of common areas
+            commonAreas1: [
+                {
+                    url: "/img/sliders/common-areas/party-room.jpg",
+                    alt: "Foto do salão de festas com sofá, televisão, mesas e cadeiras",
+                    name: "Salão de festas"
+                },
+                {
+                    url: "/img/sliders/common-areas/play-room.jpg",
+                    alt: "Foto do salão de jogos com mesa de bilhar, bar e mesa para cartas",
+                    name: "Salão de jogos"
+                },
+                {
+                    url: "/img/sliders/common-areas/theater.jpg",
+                    alt: "Foto da sala de cinema, com sofá grande, puffs e telão",
+                    name: "Cinema"
+                },
+                {
+                    url: "/img/sliders/common-areas/hot-pool.jpg",
+                    alt: "Foto da área de piscina com aquecimento",
+                    name: "Piscina"
+                },
+                {
+                    url: "/img/sliders/common-areas/gym.jpg",
+                    alt: "Foto da academia com halteres, esteiras e outros equipamentos",
+                    name: "Academia"
+                },
+                {
+                    url: "/img/sliders/common-areas/kids-place.jpg",
+                    alt: "Foto da área para crianças, com piscina de bolinhas, TV e playground",
+                    name: "Kids"
+                }
+            ],
+
+            // Second slider of common areas
+            commonAreas2: [
+                {
+                    url: "/img/sliders/common-areas/reading-place.jpg",
+                    alt: "Foto da área de leitura externa, com redes e jardim",
+                    name: "Redário"
+                },
+                {
+                    url: "/img/sliders/common-areas/playground.jpg",
+                    alt: "Foto do parquinho, com escorregador, gira-gira e outros brinquedos",
+                    name: "Playground"
+                },
+                {
+                    url: "/img/sliders/common-areas/pet-place.jpg",
+                    alt: "Foto do espaço Pet, com vários brinquedos para cães e gatos",
+                    name: "Pet Place"
+                },
+                {
+                    url: "/img/sliders/common-areas/gourmet-pool.jpg",
+                    alt: "Foto da piscina gourmet com espreguiçadeiras dentro da piscina e área de cozinha",
+                    name: "Piscina Gourmet"
+                },
+                {
+                    url: "/img/sliders/common-areas/pool.jpg",
+                    alt: "Foto da piscina externa",
+                    name: "Piscina"
+                }
+            ]
+        }
+    }
 }
