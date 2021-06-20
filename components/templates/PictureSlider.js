bb55e13e33e934c70b1fbd2f8600b1fa894f46f7
Galeno de Melo <galenodemelo@gmail.com>
2021-06-20 19:51:10 -0300

    feat: Criando bullets

--- components/templates/PictureSlider.js
+++ components/templates/PictureSlider.js
@@ -1,10 +1,28 @@
 import Image from "next/image"
+import styles from "./../../styles/components/templates/PictureSlider.module.sass"
 import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Pagination } from "swiper"
 
 export default function PictureSlider({imageList}) {
 
+    SwiperCore.use([Pagination])
+
     return (
-        <Swiper className="panel">
+        <Swiper className={[styles.pictureSlider, "panel"].join(" ")}
+                speed={300}
+                pagination={{
+                    bulletActiveClass: styles["bullet--active"],
+                    clickable: true,
+                    el: ".pagination",
+                    type: "bullets",
+                    renderBullet: (index, className) => {
+                        const name = imageList[index].name ?? ""
+
+                        return `<span class="${[styles.bullet, className].join(" ")}">
+                                    ${name !== "" ? `<b>${name}</b>` : ""}
+                                </span>`
+                    }
+                }}>
             {imageList.map((item, index) => {
                 return (
                     <SwiperSlide key={index}>
@@ -12,6 +30,8 @@ export default function PictureSlider({imageList}) {
                     </SwiperSlide>
                 )
             })}
+
+            <div className={[styles.pagination, "pagination"].join(" ")}></div>
         </Swiper>
     )
 }
