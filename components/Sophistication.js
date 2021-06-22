05c2133e7cd811c2762a9719e8ee70e717dacf85
Galeno de Melo <galenodemelo@gmail.com>
2021-06-21 23:32:53 -0300

    refactor: Aplicando PictureSlider

--- components/Sophistication.js
+++ components/Sophistication.js
@@ -1,20 +1,13 @@
-import Image from "next/image"
+import PictureSlider from "./templates/PictureSlider"
 import styles from "./../styles/components/Sophistication.module.sass"
-import { Swiper, SwiperSlide } from 'swiper/react'
 
 export default function Sophistication() {
 
     return (
         <section className={[styles.sophistication, "panel"].join(" ")}>
             <div className={styles.slider}>
-                <Swiper className="panel">
-                    <SwiperSlide>
-                        <Image src="/img/sliders/sophistication/1.jpg" layout="fill" objectFit="cover" alt="Foto do hall de entrada do empreendimento Opera à noite" loading="lazy" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Image src="/img/sliders/sophistication/2.jpg" layout="fill" objectFit="cover" alt="Foto da fachada do empreendimento Opera à noite" loading="lazy" />
-                    </SwiperSlide>
-                </Swiper>
+                <PictureSlider imageList={[{url: "/img/sliders/sophistication/1.jpg", alt: "Foto do hall de entrada do empreendimento Opera à noite"},
+                                           {url: "/img/sliders/sophistication/2.jpg", alt: "Foto da fachada do empreendimento Opera à noite"}]} />
             </div>
             <div className={styles.textbox}>
                 <hr/>
