9f9eb2ac03adfb83a393c22dee9313376e8e56c2
Galeno de Melo <galenodemelo@gmail.com>
2021-06-23 20:47:05 -0300

    feat: Criando animação de abertura

--- components/Opening.js
+++ components/Opening.js
@@ -1,9 +1,25 @@
+import { useEffect, useRef } from "react"
 import styles from "./../styles/components/Opening.module.sass"
 
 export default function Opening({setIsSoundActive}) {
+    const videoPlayer = useRef()
+    const section = useRef()
+
+    useEffect(() => {
+        videoPlayer.current.addEventListener("ended", () => {
+            section.current.classList.add(styles["opening--expanded"])
+        })
+    })
+
     return (
-        <section className={[styles.opening, "panel"].join(" ")}>
-            <h1><img src="/img/logo.svg" alt="Opera - Um empreendimento Investcorp" className={styles.logo} /></h1>
+        <section className={[styles.opening, "panel"].join(" ")} ref={section}>
+            <h1>
+                <video muted={true} controls={false} className={styles.logo} autoPlay={true} ref={videoPlayer}>
+                    <source src="/videos/logo.webm" type="video/webm"/>
+                    <source src="/videos/logo.mp4" type="video/mp4"/>
+                    <img src="/img/logo.svg" alt="Opera - Um empreendimento Investcorp" className={styles.logo} />
+                </video>
+            </h1>
 
             <nav className={styles.menu}>
                 <h6>Sound experience</h6>
