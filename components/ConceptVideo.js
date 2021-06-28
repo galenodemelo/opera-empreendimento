cfbbe3549bc95de1035c868a90942e67e34d4839
Galeno de Melo <galenodemelo@gmail.com>
2021-06-27 22:09:15 -0300

    fix: Corrigindo scroll sobre o vídeo

--- components/ConceptVideo.js
+++ components/ConceptVideo.js
@@ -7,12 +7,12 @@ export default function ConceptVideo() {
 
 
     return (
-        <section className={[styles.conceptVideo, "panel"].join(" ")}>
+        <section className={[styles.conceptVideo, "panel"].join(" ")} data-video-active={videoPlayerState}>
             {videoPlayerState &&
                 <>
-                    <iframe src="https://player.vimeo.com/video/559001048?title=0&byline=0&portrait=0&autoplay=1" 
+                    <iframe src="https://player.vimeo.com/video/565848350?title=0&byline=0&portrait=0&autoplay=1" 
                             className={styles.video}
-                            allow="autoplay; fullscreen; picture-in-picture"></iframe>
+                            allow="autoplay; fullscreen; picture-in-picture" scroll="no"></iframe>
                     <script src="https://player.vimeo.com/api/player.js"></script>
                 </>
             }
