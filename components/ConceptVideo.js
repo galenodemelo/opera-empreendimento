6b755876a75c1fc6f1955f18dbe8ce5e3c96b4f7
Galeno de Melo <galenodemelo@gmail.com>
2021-06-27 22:53:41 -0300

    fix: Animações

--- components/ConceptVideo.js
+++ components/ConceptVideo.js
@@ -2,12 +2,12 @@ import Image from "next/image"
 import styles from "./../styles/components/ConceptVideo.module.sass"
 import { useState } from "react"
 
-export default function ConceptVideo() {
+export default function ConceptVideo({active}) {
     const [videoPlayerState, setVideoPlayerState] = useState(false)
 
 
     return (
-        <section className={[styles.conceptVideo, "panel"].join(" ")} data-video-active={videoPlayerState}>
+        <section className={[styles.conceptVideo, "panel"].join(" ")} data-active={active}>
             {videoPlayerState &&
                 <>
                     <iframe src="https://player.vimeo.com/video/559001048?title=0&byline=0&portrait=0&autoplay=1" 
@@ -18,9 +18,13 @@ export default function ConceptVideo() {
             }
 
             {!videoPlayerState && (
-                <button className={styles.play} onClick={() => setVideoPlayerState(true)}>
-                    <Image src="/img/ico/play.svg" width="80" height="80" className={styles.play}/>
-                </button>
+                <>
+                    <Image src="/videos/concept-video-cover.jpg" layout="fill" objectFit="cover" className="zoom-in"/>
+
+                    <button className={styles.play} onClick={() => setVideoPlayerState(true)}>
+                        <Image src="/img/ico/play.svg" objectFit="contain" layout="fill" className={styles.play}/>
+                    </button>
+                </>
             )}
         </section>
     )
