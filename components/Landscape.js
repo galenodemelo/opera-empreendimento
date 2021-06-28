749e09be9dfc51c87a14d351136be0e2aabdc931
Galeno de Melo <galenodemelo@gmail.com>
2021-06-27 21:38:15 -0300

    feat: Adicionando animação ao bloco "Cada tom"

--- components/Landscape.js
+++ components/Landscape.js
@@ -1,10 +1,10 @@
 import Image from "next/image"
 import styles from "./../styles/components/Landscape.module.sass"
 
-export default function Landscape() {
+export default function Landscape({active}) {
     return (
-        <section className={[styles.landscape, "panel"].join(" ")}>
-            <Image src="/img/bg/landscape.jpg" layout="fill" objectFit="cover" loading="lazy" quality="75" />
+        <section className={[styles.landscape, "panel"].join(" ")} data-active={active}>
+            <Image src="/img/bg/landscape.jpg" layout="fill" objectFit="cover" loading="lazy" quality="75" className="zoom-in" />
 
             <div className={styles.lettering}>
                 <hr />
