cd2c90e7808ececa5cef514645a01bc7428faa99
Galeno de Melo <galenodemelo@gmail.com>
2021-07-11 20:13:54 -0300

    feat: Adicionando animações "slide-in" ao Textbox

--- components/Location.js
+++ components/Location.js
@@ -1,10 +1,9 @@
 import PictureSlider from "./templates/PictureSlider"
 import Textbox from "./templates/Textbox"
 
-export default function Location() {
-
+export default function Location({active}) {
     return (
-        <section className="panel panel--divided">
+        <section className="panel panel--divided" data-active={active}>
             <PictureSlider imageList={[
                 { url: "/img/sliders/tradition/daytime-building.jpg", alt: "Foto da fachada do empreendimento Opera de manhã" },
                 { url: "/img/sliders/tradition/nighttime-building.jpg", alt: "Foto da fachada do empreendimento Opera à noite" }
@@ -14,6 +13,6 @@ export default function Location() {
                              contemporânea`}
                      text="A tradição anda lado a lado com um roteiro clássico que alinha a tranquilidade de viver em um bairro nobre com as facilidades e modernidades de um empreendimento inteligente. Que tangência uma verdadeira obra de arte e valoriza cada tom dessa história." 
                      button={{text: "Localização"}}/>
-        </section>
+        </section>            
     )
 }
--- components/templates/Textbox.js
+++ components/templates/Textbox.js
@@ -1,15 +1,21 @@
 import Button from "./Button"
 import styles from "./../../styles/components/templates/Textbox.module.sass"
 
-export default function Textbox({title, text, button}) {
+export default function Textbox({title, text, button = null}) {
     return (
         <div className={styles.textbox}>
-            <hr/>
+            <hr className="slide-in-bar"/>
             <div className={styles.content}>
-                <h2>{title.split("\n").map((it, index) => (<span key={index}>{it}<br/></span>))}</h2>
-                <p>{text.split("\n").map((it, index) => (<span key={index}>{it}<br/></span>))}</p>
+                <h2 className="slide-in-bottom">
+                    {title.split("\n").map((it, index) => (<span key={index}>{it}<br/></span>))}
+                </h2>
+                <p className="slide-in-bottom">
+                    {text.split("\n").map((it, index) => (<span key={index}>{it}<br/></span>))}
+                </p>
                 {button &&
-                    <Button label={button.text}/>
+                    <div className="slide-in-bottom">
+                        <Button label={button.text} onClick={button.onClick}/>
+                    </div>
                 }
             </div>
         </div>
