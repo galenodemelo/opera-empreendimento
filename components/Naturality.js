9c1684c48dff0cf82cc387087503be4d85d21880
Galeno de Melo <galenodemelo@gmail.com>
2021-08-09 22:03:22 -0300

    feat: Fazendo a versão mobile sobre "a naturalidade"

--- components/Naturality.js
+++ components/Naturality.js
@@ -15,7 +15,7 @@ export default function Naturality({active}) {
                 </div>
             </div>
             <div className={styles.picture}>
-                <Image src="/img/bg/naturality.jpg" layout="fill" objectFit="cover" className="zoom-in"/>
+                <Image src="/img/bg/naturality.jpg" layout="fill" objectFit="contain" objectPosition="top right" className="zoom-in"/>
             </div>
         </section>
     )
